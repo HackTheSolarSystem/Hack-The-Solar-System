adf5ac08b40f31fb4f0f25683f0f4d9bd3a75dcf
github username <matthewdizon94@protonmail.com>
2019-02-10 01:18:22 -0500

    moved form

--- form.js
+++ form.js
@@ -1,10 +1,9 @@
+
  document.addEventListener("DOMContentLoaded", function(event) {
        let planets = document.getElementById('planets')
        let mass = document.getElementById('mass')
+       let distance = document.getElementById('distance')
 
-
-       let planetForDistance = document.getElementById('planet-distance')
-       let planetDistance = document.getElementById('distance-number')
        let submitButton = document.getElementById('mass-form')
        //function to take in planet selected for mass change
        function planetForm(e){
@@ -14,21 +13,17 @@
        function massForm(e){
            console.log(e.target.value)
        }
-        //function to take in planet selected for distance change
-       function planetDistanceSelect(){
+       function distanceForm(e){
            console.log(e.target.value)
        }
-       //distance change form
-       function planetDistanceInput(){
-           console.log(e.target.value)
-       }
+
        function submitMassForm(e){
            e.preventDefault()
            let planet = planets.value
            let planetMass = mass.value
-           let planetForDist = planetForDistance.value
-           let planetDist = planetDistance.value
-           console.log(planetMass,planet,planetForDist,planetDist)
+
+           let planetDist = distance.value
+           console.log(planetMass,planet,planetDist)
        }
 
 
@@ -39,10 +34,6 @@
 
        planets.addEventListener('change',planetForm)
        mass.addEventListener('change',massForm)
+       distance.addEventListener('change',distanceForm)
        submitButton.addEventListener('submit',submitMassForm)
-
-       planetForDistance.addEventListener('change',planetDistanceSelect)
-       planetDistance.addEventListener('change',planetDistanceInput)
-       planetDistanceForm.addEventListener('submit',submitDistanceForm)
-
  })
--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var sunDiameter = 150;
 var numStars = 1000;
 var earthSpeed = 0.02 * -1;
 let asteroids = [];
-
+let ee = document.getElementById("distance")
 function setup() {
     createCanvas(window.innerWidth,window.innerHeight)
     createPlanets()
@@ -142,7 +142,47 @@ function draw() {
     })
 
 }
+function hello(){
+    console.log(ee.value)
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+      let planets = document.getElementById('planets')
+      let mass = document.getElementById('mass')
+      let distance = document.getElementById('distance')
 
+      let submitButton = document.getElementById('mass-form')
+      //function to take in planet selected for mass change
+      function planetForm(e){
+          console.log(e.target.value)
+      }
+      //mass change form
+      function massForm(e){
+          console.log(e.target.value)
+      }
+      function distanceForm(e){
+          console.log(e.target.value)
+      }
+
+      function submitMassForm(e){
+          e.preventDefault()
+          let planet = planets.value
+          let planetMass = mass.value
+
+          let planetDist = distance.value
+          console.log(planetMass,planet,planetDist)
+      }
+
+
+
+      function submitDistanceForm(){
+          console.log(e.target.value)
+      }
+
+      planets.addEventListener('change',planetForm)
+      mass.addEventListener('change',massForm)
+      distance.addEventListener('change',distanceForm)
+      submitButton.addEventListener('submit',submitMassForm)
+})
 
 // mercury
 // 57.9 million km        200
